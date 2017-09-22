ee0acf937e11c71ac35ed5a1ba721446dd13ffc8
boblemarin <emeric@minimal.be>
2017-09-22 22:37:08 +0200

    Initial Commit

---
+++ Blind/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Blind
+//
+//  Created by boblemarin on 22/09/2017.
+//  Copyright © 2017 minimal.be. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Blind/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blind
+//
+//  Created by boblemarin on 22/09/2017.
+//  Copyright © 2017 minimal.be. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
