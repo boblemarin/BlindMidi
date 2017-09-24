36fa72c79cdb878342644dc3696a33c085979556
boblemarin <emeric@minimal.be>
2017-09-24 23:26:22 +0200

    implemented actual blind mode

---
+++ Blind/MidiHelper.swift
@@ -0,0 +1,14 @@
+//
+//  MidiHelper.swift
+//  Blind
+//
+//  Created by boblemarin on 24/09/2017.
+//  Copyright © 2017 minimal.be. All rights reserved.
+//
+
+import Foundation
+import CoreMIDI
+
+class MidiHelper {
+  
+}
--- Blind/ViewController.swift
+++ Blind/ViewController.swift
@@ -51,6 +51,7 @@ class ViewController: NSViewController {
 
   
   
+  @IBOutlet weak var ibBlindBox: NSButton!
   @IBOutlet weak var ibMidiSourcesTableView: NSTableView!
   
   let clientName = "BlindMidi"
@@ -60,6 +61,12 @@ class ViewController: NSViewController {
   var midiIn:MIDIPortRef = 0
   
   var isBlindModeActive = false
+  var blindModeToggleChannel:UInt8 = 177
+  var blindModeToggleCC:UInt8 = 15
+  var blindModeFaderChannel:UInt8 = 176
+  var blindModeFaderCC:UInt8 = 15
+  var lastValues = [String:(UInt8,UInt8,UInt8)]()
+  var blindValues = [String:(UInt8,UInt8,UInt8)]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -109,32 +116,97 @@ class ViewController: NSViewController {
 //    } else {
 //
 //    }
-     MIDIReceived(midiOut, pktList)
+//     MIDIReceived(midiOut, pktList)
     
-//      let packetList:MIDIPacketList = pktList.pointee
-//      var packet:MIDIPacket = packetList.packet
-//      for _ in 1...packetList.numPackets
-//      {
-//        let bytes = Mirror(reflecting: packet.data).children
-//        var dumpStr = ""
-//
-//        // bytes mirror contains all the zero values in the ridiulous packet data tuple
-//        // so use the packet length to iterate.
-//        var i = packet.length
-//        for (_, attr) in bytes.enumerated()
-//        {
-//          dumpStr += "\(attr.value as! UInt8) "
-//          i -= 1
-//          if (i <= 0)
-//          {
-//            break
-//          }
-//        }
-//
-//        print(dumpStr)
-//        packet = MIDIPacketNext(&packet).pointee
-//      }
+      let packetList:MIDIPacketList = pktList.pointee
+      var packet:MIDIPacket = packetList.packet
+      for _ in 1...packetList.numPackets
+      {
+        let bytes = Mirror(reflecting: packet.data).children
+        var midi = [UInt8]()
+
+        // bytes mirror contains all the zero values in the ridiulous packet data tuple
+        // so use the packet length to iterate.
+        var i = packet.length
+        for (_, attr) in bytes.enumerated()
+        {
+          midi.append(attr.value as! UInt8)
+//          dumpStr += "\() "
+          i -= 1
+          if (i <= 0)
+          {
+            break
+          }
+        }
+
+        handleMidi(midi)
+        packet = MIDIPacketNext(&packet).pointee
+      }
   }
+  
+  func handleMidi(_ midi:[UInt8]) {
+    var i = 0
+    while i < midi.count - 2 {
+//      var command = midi[i]
+      let v1 = midi[i]
+      let v2 = midi[i+1]
+      let v3 = midi[i+2]
+      switch (v1, v2) {
+        case (blindModeToggleChannel, blindModeToggleCC) :
+          isBlindModeActive = v3 > 0
+          DispatchQueue.main.async {
+            self.ibBlindBox.state = self.isBlindModeActive ? NSButton.StateValue.on : NSButton.StateValue.off
+          }
+          if !isBlindModeActive {
+            for (id, value) in blindValues {
+              send(value)
+              lastValues[id] = value
+            }
+            blindValues.removeAll()
+          }
+        case (blindModeFaderChannel, blindModeFaderCC):
+          if isBlindModeActive {
+            for (id, value) in blindValues {
+              if let lastValue = lastValues[id] {
+                mix(lastValue, with: value, q: v3)
+              }
+            }
+          }
+        
+        default:
+          let values = (v1, v2, v3)
+          if isBlindModeActive {
+            blindValues["\(v1)-\(v2)"] = values
+//            print("MIDI IN : \(values)")
+          } else {
+            lastValues["\(v1)-\(v2)"] = values
+            send(values)
+          }
+      }
+      
+      i += 3
+    }
+  }
+  
+  func mix(_ lastValues:(UInt8, UInt8, UInt8), with blindValues:(UInt8, UInt8, UInt8), q:UInt8) {
+    let mixFactor = Float(q) / 128
+    let mixValue = UInt8( Float(lastValues.2) * (1 - mixFactor) + Float(blindValues.2) * mixFactor)
+    send((lastValues.0, lastValues.1, mixValue))
+  }
+  
+  func send(_ values:(UInt8, UInt8, UInt8)) {
+    var packet = MIDIPacket()
+    packet.timeStamp = 0
+    packet.length = 3
+    // packet.data = midiDataTuple
+    packet.data.0 = values.0
+    packet.data.1 = values.1
+    packet.data.2 = values.2
+    
+    var midiPacketList = MIDIPacketList(numPackets: 1, packet: packet)
+    MIDIReceived(midiOut, &midiPacketList)
+  }
+  
 }
 
 
