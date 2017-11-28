b604cdfa188519e39937ce768bec96cc82a8ca5a
boblemarin <emeric@minimal.be>
2017-11-28 15:53:50 +0100

    add custom nswindow subclass to avoid warning message

---
+++ Blind/NSFuckingWindow.swift
@@ -0,0 +1,14 @@
+//
+//  NSFuckingWindow.swift
+//  BlindMIDI
+//
+//  Created by boblemarin on 28/11/2017.
+//  Copyright © 2017 minimal.be. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class NSFuckingWindow:NSWindow {
+  // just to silence Xcode 9.1 warnings
+}
